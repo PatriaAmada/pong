4afeac47020cda9a113fd625b864860817f5b4b4
PatriaAmada <80002889+PatriaAmada@users.noreply.github.com>
2025-03-23 14:39:03 -0700

    Add files via upload

---
+++ Paddle.lua
@@ -0,0 +1,45 @@
+-- Paddle class
+
+Paddle = Class{}
+    --[[The `init` function on our class is called just once, when the object
+    is first created. Used to set up all variables in the class and get it
+    ready for use.
+    Our Paddle should take an X and a Y, for positioning, as well as a width
+    and height for its dimensions.
+    Note that `self` is a reference to *this* object, whichever object is
+    instantiated at the time this function is called. Different objects can
+    have their own x, y, width, and height values, thus serving as containers
+    for data. In this sense, they're very similar to structs in C.]]
+
+function Paddle:init(x, y, width, height)
+    self.x = x
+    self.y = y
+    self.width = width
+    self.height = height
+    self.dy = 0
+end
+
+function Paddle:update(dt)
+
+    if self.dy < 0 then
+        -- add negative paddle speed to current Y scaled by deltaTime
+        -- put a limit on the movement so the paddle wont go above the screen
+        self.y = math.max(0, self.y + self.dy * dt)
+    else
+        -- add positive paddle speed to current Y scaled by deltaTime
+        -- put a limit on the movement so the paddle wont go below the screen
+        self.y = math.min(VIRTUAL_HEIGHT - self.height, self.y + self.dy * dt)
+    end
+end
+
+
+--[[
+    To be called by our main function in `love.draw`, ideally. Uses
+    LÖVE2D's `rectangle` function, which takes in a draw mode as the first
+    argument as well as the position and dimensions for the rectangle. To
+    change the color, one must call `love.graphics.setColor`. As of the
+    newest version of LÖVE2D, you can even draw rounded rectangles!
+]]
+function Paddle:render()
+    love.graphics.rectangle('fill', self.x, self.y, self.width, self.height)
+end
---
+++ README.md
@@ -0,0 +1,2 @@
+# PatriaAmada
+#this was a project I did to learn more about how to make games
